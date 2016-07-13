4b8ec4b21a3bc6bcbdd972a42455171eef47c817
amirali <yones.gholami45@gmail.com>
2016-07-13 21:10:37 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,8 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 188548712  --ID ADMIN ایدی خود را اینجا قرار دهید
+local bot_api_key = "" 216815498:AAHh8O0RfPa36J82j5RIIh_QPeF-ZulSus4
+local You = 188548712  195432079
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
@@ -33,7 +33,7 @@ local help = [[
 `/id` ✴️
 ایدی
 ➖➖➖➖➖➖➖➖➖➖➖
-M.KH @cruel0098
+Daren.Shown
 ]]--
 -------
 
